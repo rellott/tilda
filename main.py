13e6f37d936175ead90cadd1ce6a4d7f89edf105
rellott <41116014+rellott@users.noreply.github.com>
2023-03-19 19:58:57 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello World!')
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/tilda-webhook', methods=['POST'])
+def tilda_webhook():
+    data = request.json  # get the data sent by Tilda
+    # process the data as needed
+    # for example, you can save it to a database or send it to another service
+    print(data)
+    return 'OK'
